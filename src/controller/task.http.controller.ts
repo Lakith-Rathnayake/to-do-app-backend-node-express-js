be9188d2bbdd8d9e3693405badb299c6c0391bfa
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-12-29 20:10:09 +0530

    Implement getAllTasks()

--- src/controller/task.http.controller.ts
+++ src/controller/task.http.controller.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import {Request, Response} from "express-serve-static-core";
+import {Pool} from 'pg';
 
 
 const controller = Router();
@@ -11,12 +12,27 @@ controller.delete('/:id', deleteTask);
 
 export {controller as TaskHttpController};
 
-function getAllTasks(req: Request, res: Response) {
+const pool = new Pool({
+    database: "dep11_todo_app",
+    host: "localhost",
+    user: 'postgres',
+    port: 5432,
+    password: "postgres",
+    max: 10
+})
 
+async function getAllTasks(req: Request, res: Response) {
+    if(!req.query.email) res.sendStatus(400);
+    const connection = await pool.connect();
+    const taskList = await connection.query("SELECT * FROM task WHERE email=$1 ORDER BY id", [req.query.email]);
+    res.json(taskList.rows);
+    connection.release();
 }
 
-function postTask(req: Request, res: Response) {
+async function postTask(req: Request, res: Response) {
+    const connection = await pool.connect();
 
+    connection.release();
 }
 
 function updateTask(req: Request, res: Response) {

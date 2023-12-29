969d959ad6054cf4e3f1a76b1a8fe8a7b4483e98
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-12-29 17:02:15 +0530

    Add TaskTO class

--- src/controller/task.http.controller.ts
+++ src/controller/task.http.controller.ts
@@ -1,10 +1,28 @@
 import {Router} from 'express';
+import {Request, Response} from "express-serve-static-core";
+
 
 const controller = Router();
 
-controller.get("/", );
-controller.post('/', );
-controller.patch('/', );
-controller.delete('/', );
+controller.get("/", getAllTasks);
+controller.post('/', postTask);
+controller.patch('/:id', updateTask);
+controller.delete('/:id', deleteTask);
 
 export {controller as TaskHttpController};
+
+function getAllTasks(req: Request, res: Response) {
+
+}
+
+function postTask(req: Request, res: Response) {
+
+}
+
+function updateTask(req: Request, res: Response) {
+
+}
+
+function deleteTask(req: Request, res: Response) {
+
+}
---
+++ src/to/TaskTO.ts
@@ -0,0 +1,8 @@
+export class TaskTO {
+
+    constructor(public id: number,
+                public description: string,
+                public status: boolean,
+                public email: string) {
+    }
+}
